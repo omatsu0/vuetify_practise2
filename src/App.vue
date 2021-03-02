99b1d7c55c0fce239feb219d61ef54d36d392092
“sss-o2” <your@example.com>
2021-03-03 00:13:30 +0900

    add header component

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,18 @@
 <template>
   <v-app>
-    <header>
-      <v-app-bar
-        app
-        dark
-      >
-        <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-        <v-toolbar-title>MyPortfolioSite</v-toolbar-title>
-      </v-app-bar>
-      <!-- ここから -->
-      <v-navigation-drawer
-        v-model="drawer"
-        fixed
-        temporary
-      >
-        <v-list
-          nav
-          dense
-        >
-          <v-list-item-group>
-            <v-list-item
-              v-for="(menuItem, index) in menuItems"
-              :key="index"
-            >
-              <v-list-item-title>{{ menuItem.name }}</v-list-item-title>
-            </v-list-item>
-          </v-list-item-group>
-        </v-list>
-      </v-navigation-drawer>
-      <!-- ここまで -->
-    </header>
+    <AppHeader/>
   </v-app>
 </template>
  
 <script>
-import constants from './common/constants' // 今回追加
+import AppHeader from './components/AppHeader.vue'
  
 export default {
-  data () {
-    return {
-      drawer: false,
-      menuItems: constants.menuItems // 今回追加
-    }
+  name: 'App',
+  components: {
+    AppHeader
   }
 }
 </script>
+
  
-<style></style>
