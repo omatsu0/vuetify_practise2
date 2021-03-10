27faead005f91b18a1cf02cdb9655a3565e5c5f9
“sss-o2” <your@example.com>
2021-03-10 22:40:56 +0900

    sass loader downgreade

--- src/App.vue
+++ src/App.vue
@@ -14,4 +14,24 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.v-toolbar__title {
+  overflow: visible !important;
+  margin-right: 50px !important;
+}
+ 
+.v-app-bar__nav-icon {
+  @include display_pc {
+    display: none !important;
+  }
+}
+.v-tabs {
+  display: none;
+ 
+  @include display_pc {
+    display: block !important;
+  }
+}
+</style>
  
--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,11 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
+  css: {
+    loaderOptions: {
+      scss: {
+        prependData: '@import "./src/styles/common/common.scss";'
+      }
+    }
+  }
 }
