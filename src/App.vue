e7e50e8b0d2503ab9b1d586917cbd011920e5d14
“sss-o2” <your@example.com>
2021-03-12 23:40:03 +0900

    add navmenu

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <v-app>
     <AppHeader/>
+    <v-content>
+      <Home/>
+    </v-content>
     <AppFooter/>
   </v-app>
 </template>
  
 <script>
 import AppHeader from './components/AppHeader.vue'
+import Home from './components/Home.vue'
 import AppFooter from './components/AppFooter.vue'
 
 export default {
   name: 'App',
   components: {
     AppHeader,
+    Home,
     AppFooter
   }
 }
---
+++ src/components/HomeHero.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="home-hero">
+    <v-container fluid fill-height class="home-hero__content">
+      <v-row>
+        <v-col class="home-hero__content-text">
+          <p>Welcome to my website.</p>
+          <p>I'm a enginner.</p>
+        </v-col>
+      </v-row>
+    </v-container>
+  </section>
+</template>
+<style lang="scss" scoped>
+.home-hero__content {
+  background: url("../assets/img/home-hero.jpg");
+  background-size: cover;
+  background-position: center center;
+  width: 100%;
+  height: 50vh;
+ 
+ @include display_pc{
+   height: 100vh;
+ }
+  &-text {
+    color: white;
+    text-align: center;
+    font-size: 28px;
+    font-weight: bold;
+
+    @include display_pc{
+      font-size: 40px;
+    }
+  }
+}
+</style>
