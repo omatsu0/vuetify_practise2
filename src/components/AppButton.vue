c8ab4490ebbcd2f209326911d91cff18890c0373
“omatsu0“ <62799261+omatsu0@users.noreply.github.com>
2021-03-16 01:30:24 +0900

    add AppButton.vue

---
+++ src/components/AppButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-row class="button">
+    <v-col>
+      <v-btn
+        large
+        color="error"
+        min-width="300"
+        :to="url"
+      >
+        ＞＞{{buttonText}}
+      </v-btn>
+    </v-col>
+  </v-row>
+</template>
+<style lang="scss">
+.button {
+  text-align: center;
+}
+</style>
