7c4ec0a5d2babc62a523c0867d5001ce463a74cc
“omatsu0“ <62799261+omatsu0@users.noreply.github.com>
2021-03-23 23:16:30 +0900

    addHomeServise.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,18 +3,21 @@
     <HomeHero/>
     <HomeAbout/>
     <HomeWork/>
+    <HomeService/>
   </section>
 </template>
 <script>
 import HomeHero from './HomeHero.vue'
 import HomeAbout from './HomeAbout.vue'
 import HomeWork from './HomeWork.vue'
- 
+import HomeService from './HomeService.vue'
+
 export default {
   components: {
     HomeHero,
     HomeAbout,
-    HomeWork
+    HomeWork,
+    HomeService
   }
 }
 </script>
